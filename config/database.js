7846694cfbbed45c8d757655e109747b2857c44c
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2025-06-08 15:19:33 -0300

    commit privio al deploy en vercel

--- config/database.js
+++ config/database.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
-import  'dotenv/config.js';
-mongoose.connect(process.env.DATABASE_URL)
-.then( ()=>{console.log("database conected")})
-.catch(()=>{console.log("database connection failed")})
+import "dotenv/config.js";
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("🔍database conected");
+  })
+  .catch((err) => {
+    console.error("❌ Error al conectar a la base de datos:", err.message);
+  });
