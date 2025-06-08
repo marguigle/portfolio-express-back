8f76f0ba4886eadec3776d06eb4e3071538fab0a
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2025-06-08 16:24:21 -0300

    se hace de nuevo el vercel.json

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import "../config/database.js";
+import personaRouter from "../routes/personaRouter.js";
+import expLaboralRouter from "../routes/expLaboralRouter.js";
+import estudiosRouter from "../routes/estudiosRouter.js";
+import actDocenteRouter from "../routes/actDocenteRouter.js";
+import congresosRouter from "../routes/congresosRouter.js";
+import hardSkillsRouter from "../routes/hardSkillsRouter.js";
+import proyectosRouter from "../routes/proyectosRouter.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("bienvenido a mi servidor");
+});
+
+app.use("/api", personaRouter);
+app.use("/api", expLaboralRouter);
+app.use("/api", estudiosRouter);
+app.use("/api", actDocenteRouter);
+app.use("/api", congresosRouter);
+app.use("/api", hardSkillsRouter);
+app.use("/api", proyectosRouter);
+
+// 💡 Exportá el handler de Express para Vercel
+export default app;
